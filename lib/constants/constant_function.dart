8fc656b65070cbc01b19ad05ea45567204b7cd48
MyselfMaaz <muazahmad110@gmail.com>
2024-08-23 12:41:52 +0500

    Type Your Key

--- lib/constants/constant_function.dart
+++ lib/constants/constant_function.dart
@@ -5,8 +5,8 @@ import 'package:http/http.dart' as http;
 class ConstantFunctions {
   static Future<List<Map<String, dynamic>>> getResponse(String value) async {
     List<Map<String, dynamic>> recipe = [];
-    String id = '8df4d003';
-    String key = '34c9b4aca9d5150f2ab2dbcc897ce2df';
+    String id = '';
+    String key = '';
     String api =
         'https://api.edamam.com/search?q=$value&app_id=$id&app_key=$key&from=0&to=20&calories=591-722&health=alcohol-free';
 
